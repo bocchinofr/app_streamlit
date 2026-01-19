7205b46cb56026abff4339d2510f6971dcf1e183
bocchinofr <bocchinofr@gmail.com>
2026-01-19 09:28:25 +0100

    Update strategia.py

--- pages/strategia.py
+++ pages/strategia.py
@@ -40,7 +40,7 @@ def load_data():
 
 df = load_data()
 
-st.text("La strategia prevede un solo ingresso SHORT in base ai parametri definiti in sidebar. \n Solo una volta raggiunto il livello di entry sarà attivata l'operazione e saranno verificati TP e SL")
+st.text("La strategia prevede un solo ingresso SHORT in base ai parametri definiti in sidebar. \nSolo una volta raggiunto il livello di entry sarà attivata l'operazione e saranno verificati TP e SL")
 
 
 # region Filtri
