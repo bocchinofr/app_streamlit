1b2081b75f647d339046e5c32697b9f59f12d353
bocchinofr <bocchinofr@gmail.com>
2026-02-06 09:54:15 +0100

    Update app copia.py

--- pages/app copia.py
+++ pages/app copia.py
@@ -761,7 +761,9 @@ styled_df = filtered_sorted.style.set_properties(**{
     "text-align": "left"
 })
 
-st.dataframe(filtered_sorted, use_container_width=True)
+st.dataframe(styled_df, use_container_width=True)
+
+#st.dataframe(filtered_sorted, use_container_width=True)
 st.caption(f"Sto mostrando {len(filtered_sorted)} record filtrati su {len(df)} totali.")
 
 
