6040d55626d293a4dff733b2a92cac09164d3be4
bocchinofr <bocchinofr@gmail.com>
2026-02-06 10:13:38 +0100

    Update app copia.py

--- pages/app copia.py
+++ pages/app copia.py
@@ -768,11 +768,16 @@ for col in percent_cols_display:
         )
 
 
-
 display_df = filtered_sorted.copy()
 
 for col in display_df.columns:
     display_df[col] = display_df[col].astype(str)
+
+
+for col in percent_cols_display:
+    if col in display_df.columns:
+        display_df[col] = display_df[col].str.replace(r"\.0$", "", regex=True)
+
 
 st.dataframe(display_df, use_container_width=True)
 
