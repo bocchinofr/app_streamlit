2dc659447be7113c5dd60fc510bb3ae3d0e1db3a
bocchinofr <bocchinofr@gmail.com>
2026-02-11 10:55:45 +0100

    Update multigapper.py

--- pages/multigapper.py
+++ pages/multigapper.py
@@ -512,6 +512,13 @@ def ci_box(df, label):
     st.plotly_chart(fig, use_container_width=True)
 
 
+# Creiamo il grafico intraday
+fig_intraday = intraday_bar(oh_red, oh_green)
+
+# Visualizziamo in Streamlit
+st.plotly_chart(fig_intraday, use_container_width=True)
+
+
 # endregion
 
 
