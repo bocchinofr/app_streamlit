dc664cc330b2e84cce2b30e4f364c799b99c9450
bocchinofr <bocchinofr@gmail.com>
2026-02-11 09:17:59 +0100

    Update multigapper.py

--- pages/multigapper.py
+++ pages/multigapper.py
@@ -677,7 +677,7 @@ st.markdown(top_html, unsafe_allow_html=True)
 
 import plotly.graph_objects as go
 
-metrics = ["day_high_pct", "day_low_pct", "day_close_pct"]
+metrics = ["%OH", "%OL", "day_close_pct"]
 labels  = ["High %", "Low %", "Close %"]
 
 green_means = [green_df[m].mean() for m in metrics]
